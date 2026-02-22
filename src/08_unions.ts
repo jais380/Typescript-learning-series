bff5a50b51694fec696055a3f5f81434fb1f2e48
JUDE <judeani60@gmail.com>
2026-02-22 17:35:13 +0100

    feat: add unions

---
+++ src/08_unions.ts
@@ -0,0 +1,39 @@
+//union -> |
+//value can be either this OR that
+
+function printId(id: string | number){
+    if(typeof id === "string"){
+        console.log(id.toUpperCase)
+    }else {
+        id.toFixed(2);
+    }
+
+}
+
+
+//object union
+
+type Admin = {role: 'Admin', permissions : string[]}
+type Customer = {role: 'Customer', loyaltyPoints : number}
+
+function describeUser(u : Admin | Customer) {
+    if(u.role === 'Admin'){
+        console.log(u.permissions);
+    }else {
+        console.log(u.loyaltyPoints);
+    }
+}
+
+
+function describeUserWithInOperator(u: Admin | Customer) {
+    if('permissions' in u){
+        console.log(u.role, 'Admin user');
+    } else {
+        console.log(u.loyaltyPoints);
+    }
+}
+
+//array of unions and union of arrays
+const arrOfUnion: (string | number)[]= ["a",1,"b","2"]
+
+const unionOfArrays: string[] | number[] = Math.random() > 0.1 ? ["x", "y"] : [1, 2]
